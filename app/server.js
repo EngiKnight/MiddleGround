504c4e879d1f4a0b9f87a988c372c8a2e545f27c
Joshua Smith <jcs474@drexel.edu>
2025-08-02 23:03:31 -0400

    Added a sample route and html page for the login screen

--- app/server.js
+++ app/server.js
@@ -4,6 +4,12 @@ const app = express();
 const port = 3000;
 const hostname = "localhost";
 
+app.get("/login.html", (req, res, next) => {
+    console.log("log in");
+    next();
+});
+
+
 app.use(express.static("public"));
 
 app.use(express.json());
