b11fd8b304b4b188a062ec5815c573a79c24abb4
jmt7803 <turnerjaden@gmail.com>
2025-08-04 22:55:45 -0400

    added freeform searching for locations with nonatim api

--- app/public/API test/api.js
+++ app/public/API test/api.js
@@ -1,21 +1,18 @@
-// Basic Nonatim API request using latitude and longitude coordinates
-let button = document.getElementById("submit");
+// Basic Nonatim API requests with lat/lon search and freeform search
+let coordsButton = document.getElementById("submitCoords");
+let searchButton = document.getElementById("submitAddress");
 let latitude = document.getElementById("lat");
-let longitude = document.getElementById("long");
+let longitude = document.getElementById("lon");
 let loc = document.getElementById("location");
+let coordsCode = document.getElementById("coordsCode");
+let coordsMessage = document.getElementById("coordsMsg");
+let search = document.getElementById("search");
+let searchTable = document.getElementById("tbody");
+let searchCode = document.getElementById("searchCode");
+let searchMessage = document.getElementById("searchMsg");
 
 const revURL = "https://nominatim.openstreetmap.org/reverse?format=jsonv2";
-
-function revSearch(lat, lon) {
-  let url = new URL(revURL + "&lat=" + lat + "&lon=" + lon);
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    })
-    .catch(console.error);
-}
+const searchURL = "https://nominatim.openstreetmap.org/search?format=jsonv2";
 
 /*
 revSearch returns a jsonv2 with the following example format
@@ -47,18 +44,120 @@ revSearch returns a jsonv2 with the following example format
 }
 */
 
-button.addEventListener("click", () => {
+function revSearch(lat, lon) {
+  let url = new URL(revURL + "&lat=" + lat + "&lon=" + lon);
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+//freeSearch returns an array of json objects with the same format, but in indices 0-n, where n is the number of results minus 1.
+//freeSearch URL can be modified to add a limit parameter (such as &limit=1, would return same result as revSearch)
+function freeSearch(query) {
+  let transformedQuery = query.replace(/ /g, "+");
+  let url = new URL(searchURL + "&q=" + transformedQuery);
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+coordsButton.addEventListener("click", () => {
   loc.textContent = "";
+  coordsMessage.textContent = "";
   let latVal = latitude.value;
   let lonVal = longitude.value;
 
-  // Use .then() to handle result
-  revSearch(latVal, lonVal).then((result) => {
-    if (result) {
-      console.log(latVal);
-      console.log(lonVal);
-      console.log(result.display_name);
-      loc.textContent = result.display_name;
-    }
-  });
+  // use .then() to handle result
+  revSearch(latVal, lonVal)
+    .then((result) => {
+      if (result) {
+        loc.textContent = result.display_name;
+      }
+
+      if (result.error) {
+        let code = "Error Code: ";
+        let message = "Error Message: ";
+        if (result.error.code || result.error.message) {
+          code += result.error.code;
+          message += result.error.message;
+        } else {
+          code += "400";
+          message = result.error;
+        }
+        coordsCode.textContent = code;
+        coordsMessage.textContent = message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      coordsMessage.textContent = "ERROR: Check console for info";
+    });
+});
+
+searchButton.addEventListener("click", () => {
+  let searchText = search.value;
+
+  searchCode.textContent = "";
+  searchMessage.textContent = "";
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  // .then() to handle result
+  freeSearch(searchText)
+    .then((result) => {
+      if (result) {
+        for (let i = 0; i < result.length; i++) {
+          let tr = document.createElement("tr");
+          let resultNum = document.createElement("td");
+          let resultLoc = document.createElement("td");
+          let lat = document.createElement("td");
+          let lon = document.createElement("td");
+
+          resultNum.textContent = `${i + 1}`;
+          resultLoc.textContent = result[i].display_name;
+          lat.textContent = result[i].lat;
+          lon.textContent = result[i].lon;
+
+          tr.append(resultNum);
+          tr.append(resultLoc);
+          tr.append(lat);
+          tr.append(lon);
+          tbody.append(tr);
+        }
+      }
+      if (result.error) {
+        let code = "Error Code: ";
+        let message = "Error Message: ";
+        if (result.error.code || result.error.message) {
+          code += result.error.code;
+          message += result.error.message;
+        } else {
+          code += "400";
+          message = result.error;
+        }
+        searchCode.textContent = code;
+        searchMessage.textContent = message;
+      }
+
+      if (result.length <= 0) {
+        searchCode.textContent = "Error Code: 400";
+        searchMessage.textContent =
+          "Error Message: No results returned, please try again with a different search";
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      searchMessage.textContent = "ERROR: Check console for info";
+    });
 });
